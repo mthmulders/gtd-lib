aa04859b7661356ba6882f7cc785ed1fef3584da
Maarten Mulders <m.th.mulders@xs4all.nl>
2020-01-07 23:17:43 +0100

    Configure test coverage reporting

--- jest.config.js
+++ jest.config.js
@@ -35,7 +35,7 @@ module.exports = {
   coverageReporters: [
   //   "json",
        "text",
-  //   "lcov",
+       "lcov",
   //   "clover"
   ],
 
@@ -158,7 +158,7 @@ module.exports = {
   // testRegex: [],
 
   // This option allows the use of a custom results processor
-  // testResultsProcessor: null,
+  testResultsProcessor: 'jest-sonar-reporter',
 
   // This option allows use of a custom test runner
   // testRunner: "jasmine2",
